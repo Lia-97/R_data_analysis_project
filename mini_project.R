2be5dab2363aac725490b57e842813da13a24151
Jihye Lee <zhezhe0206@naver.com>
2020-10-06 16:43:21 +0900

    make graph

--- mini_project.R
+++ mini_project.R
@@ -1,59 +1,83 @@
 #정답율의 비율을 처리한다.
 library(dplyr)
+library(ggplot2)
 getwd()
 setwd("C:/mini_project")
-exam <- read.csv("data/1_수학.csv")
-head(exam)
 
-# answer_percent의 %제거, 숫자로 변환
-exam$answer_percent=as.numeric(gsub("%","",exam$answer_percent))
-head(exam)
+rm(list=ls())
+src_dir <- c("C:/mini_project/data")
+src_dir
 
-a = exam %>% 
-  filter(answer_percent >= 90) %>% 
-  tally()*100/exam$problem_num 
-b = exam %>% 
-  filter(answer_percent >= 80 & answer_percent < 90) %>% 
-  tally()*100/exam$problem_num
-c = exam %>% 
-  filter(answer_percent >= 70 & answer_percent < 80) %>% 
-  tally()*100/exam$problem_num
-d = exam %>% 
-  filter(answer_percent >= 60 & answer_percent < 70) %>% 
-  tally()*100/exam$problem_num
-e = exam %>% 
-  filter(answer_percent >= 50 & answer_percent < 60) %>% 
-  tally()*100/exam$problem_num
-f = exam %>% 
-  filter(answer_percent >= 40 & answer_percent < 50) %>% 
-  tally()*100/exam$problem_num
-g = exam %>% 
-  filter(answer_percent >= 30 & answer_percent < 40) %>% 
-  tally()*100/exam$problem_num
-h = exam %>% 
-  filter(answer_percent >= 20 & answer_percent < 30) %>% 
-  tally()*100/exam$problem_num
-i = exam %>% 
-  filter(answer_percent >= 10 & answer_percent < 20) %>% 
-  tally()*100/exam$problem_num
-j = exam %>% 
-  filter(answer_percent >= 0 & answer_percent < 10) %>% 
-  tally()*100/exam$problem_num
+src_file <- list.files(src_dir)
+src_file
+
+src_file_cnt <- length(src_file)
+src_file_cnt
+
+for(i in 1:src_file_cnt){
+  dataset <- read.table(
+    paste(src_dir,"/",src_file[i],sep=""),
+    sep=",",header=T,stringsAsFactors = F)
+  # answer_percent의 %제거, 숫자로 변환
+  dataset$answer_percent=as.numeric(gsub("%","",dataset$answer_percent))
+  answer_percent <- round(dataset$answer_percent,2)
+  head(dataset)
+  
+  #전체 문제 중 각 정답율이 차지하는 문제 비율을 구한다.
+  a = dataset %>% 
+    filter(answer_percent >= 90) %>% 
+    tally()*100/dataset$problem_num 
+  b = dataset %>% 
+    filter(answer_percent >= 80 & answer_percent < 90) %>% 
+    tally()*100/dataset$problem_num
+  c = dataset %>% 
+    filter(answer_percent >= 70 & answer_percent < 80) %>% 
+    tally()*100/dataset$problem_num
+  d = dataset %>% 
+    filter(answer_percent >= 60 & answer_percent < 70) %>% 
+    tally()*100/dataset$problem_num
+  e = dataset %>% 
+    filter(answer_percent >= 50 & answer_percent < 60) %>% 
+    tally()*100/dataset$problem_num
+  f = dataset %>% 
+    filter(answer_percent >= 40 & answer_percent < 50) %>% 
+    tally()*100/dataset$problem_num
+  g = dataset %>% 
+    filter(answer_percent >= 30 & answer_percent < 40) %>% 
+    tally()*100/dataset$problem_num
+  h = dataset %>% 
+    filter(answer_percent >= 20 & answer_percent < 30) %>% 
+    tally()*100/dataset$problem_num
+  j = dataset %>% 
+    filter(answer_percent >= 10 & answer_percent < 20) %>% 
+    tally()*100/dataset$problem_num
+  k = dataset %>% 
+    filter(answer_percent >= 0 & answer_percent < 10) %>% 
+    tally()*100/dataset$problem_num
+  
+  #이를 테이블로 만든다.
+  tab <- data.frame(tag = c('90~100','80~90','70~80',
+                            '60~70','50~60','40~50',
+                            '30~40','20~30','10~20',
+                            '0~10'),  
+                    percent = rbind(round(a,2),round(b,2),round(c,2),
+                                    round(d,2),round(e,2),round(f,2),
+                                    round(g,2),round(h,2),round(j,2),round(k,2)))
+  names(tab) <- c("tag", "percent")
+  print(head(tab))
+  
+  
+  #테이블을 그래프로 그린다.
+  baekjoon <- ggplot(tab, aes(x=tag,y=round(percent,2),group=1))+
+    geom_point(color="steelblue",stroke=1)+
+    geom_line(color="steelblue")+
+    geom_label(aes(label=percent), nudge_y=1)+
+    labs(x="정답율(%)", y="문제 비율(%)", title=gsub("[[:digit:][:punct:][:lower:][:upper:]]","",src_file[i]))
+  print(baekjoon)
+  ggsave(paste0(gsub("[[:digit:][:punct:][:lower:][:upper:]]","",src_file[i]),".png"))
+}
 
 
-tab <- data.frame(tag = c('90~100','80~90','70~80',
-                          '60~70','50~60','40~50',
-                          '30~40','20~30','10~20',
-                          '0~10'),  
-                    percent = rbind(a,b,c,d,e,f,g,h,i,j))
-names(tab) <- c("tag", "percent")
-View(tab)
 
-library(ggplot2)
-tab
-baekjoon <- ggplot(tab, aes(x=tag,y=percent,group=1))+geom_point(color="steelblue",stroke=1)+
-  geom_line(color="steelblue")+geom_label(aes(label=percent), nudge_y=1)+
-  labs(x="정답율(%)", y="문제 비율(%)", title="수학")
-baekjoon
 
 
